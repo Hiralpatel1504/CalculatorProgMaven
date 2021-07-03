8a31fc3fd18c61f77cea23e5bb7b4ebbe2a8c3bc
Hiral Patel <patelhiral1504@gmail.com>
2021-07-03 09:58:45 -0400

    first commit

---
+++ src/main/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public void test() {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name =  s.next();
+		System.out.println("you name is: "+name);
+	}
+
+}
